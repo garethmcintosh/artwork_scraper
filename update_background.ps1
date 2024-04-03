8e94aac428d8439e0ee7c0c5caf2c683e842faba
garethmcintosh <gazzadamazza@gmail.com>
2024-04-03 16:20:07 +1300

    Add powershell script to update background on Windows OS

---
+++ update_background.ps1
@@ -0,0 +1,42 @@
+$ArtworkFolder = ".\artwork"
+
+if (-Not (Test-Path -Path $ArtworkFolder)) {
+    Write-Host "Artwork folder not found: $ArtworkFolder"
+    exit
+}
+
+$RandomImage = Get-ChildItem -Path $ArtworkFolder -File | Get-Random
+
+if ($null -eq $RandomImage) {
+    Write-Host "No images found in the artwork folder."
+    exit
+}
+
+$ScreenResolution = "1920x1080"
+
+$TimeStamp = Get-Date -Format "yyyyMMddHHmmss"
+$ResizedImage = ".\temp\resized_image_$TimeStamp.png"
+
+magick convert "$($RandomImage.FullName)" -resize "$ScreenResolution>" "$ResizedImage"
+
+if (-Not (Test-Path -Path $ResizedImage)) {
+    Write-Host "Failed to create resized image."
+    exit
+}
+
+$BGColor = magick convert "$ResizedImage" -format "%[pixel:p{0,0}]" info:
+
+$BGColorImage = ".\temp\bg_color_$TimeStamp.png"
+magick convert -size "$ScreenResolution" "xc:$BGColor" $BGColorImage
+
+magick convert $BGColorImage $ResizedImage -gravity center -composite $ResizedImage
+
+Add-Type -TypeDefinition @"
+using System;
+using System.Runtime.InteropServices;
+public class Wallpaper {
+    [DllImport("user32.dll", CharSet = CharSet.Auto)]
+    public static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+}
+"@
+[Wallpaper]::SystemParametersInfo(20, 0, $RandomImage.FullName, 3)
